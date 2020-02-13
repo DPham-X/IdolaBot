0cfb2d3fd1b3dee26dafe248a9efd043ddfc122b
Dzuy Pham <pham.dzuy@gmail.com>
2020-02-13 18:12:41 +1100

    Remove prefix

--- idola/idola_bot.py
+++ idola/idola_bot.py
@@ -7,6 +7,6 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="!idola ", description="""IDOLA BOT""")
+bot = commands.Bot(command_prefix="!", description="""IDOLA BOT""")
 bot.load_extension("cogs.idola")
 bot.run(TOKEN)
