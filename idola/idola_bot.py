0e58139aa6a0126f6ac07c4223944d732f699c75
Dzuy Pham <pham.dzuy@gmail.com>
2020-02-16 21:23:24 +1100

    Made discord prefix configurable

--- idola/idola_bot.py
+++ idola/idola_bot.py
@@ -6,7 +6,10 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("DISCORD_TOKEN")
+PREFIX = os.getenv("DISCORD_PREFIX")
+if not PREFIX:
+	raise Exception("Discord prefix must be defined")
 
-bot = commands.Bot(command_prefix="!", description="""IDOLA BOT""")
+bot = commands.Bot(command_prefix=PREFIX, description="""IDOLA BOT""")
 bot.load_extension("cogs.idola")
 bot.run(TOKEN)
