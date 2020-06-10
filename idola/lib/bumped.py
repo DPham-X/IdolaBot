7ba5a518a00823a053d4256388be6fd21643e8ec
Dzuy Pham <pham.dzuy@gmail.com>
2020-06-10 23:58:27 +1000

    Use pyppeteer instead of aiohttp as not all symbols were being retrieved

--- idola/lib/bumped.py
+++ idola/lib/bumped.py
@@ -4,10 +4,10 @@ import logging
 import re
 from dataclasses import dataclass, field
 
-import aiohttp
 import textdistance
 from bs4 import BeautifulSoup
 from fuzzywuzzy import fuzz, process
+from pyppeteer import launch
 
 logger = logging.getLogger(f"idola.{__name__}")
 
@@ -78,97 +78,104 @@ class BumpedParser(object):
         await self.import_soul_symbols()
 
     async def import_weapon_symbols(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(WEAPON_DATABASE_URL) as r:
-                html = await r.text()
-        soup = BeautifulSoup(html, "html.parser")
+        browser = await launch(headless=True)
+        try:
+            page = await browser.newPage()
+            await page.goto(WEAPON_DATABASE_URL)
+            html = await page.content()
+            soup = BeautifulSoup(html, "html.parser")
 
-        # Remove javascript and css from html
-        for script in soup(["script", "style"]):
-            script.extract()
+            # Remove javascript and css from html
+            for script in soup(["script", "style"]):
+                script.extract()
 
-        tables = soup.findAll("table", {"class": "pso2"})
-        for table in tables:
-            max_table_len = len(table.findAll("tr"))
-            for row in table.findAll("tr")[1:max_table_len]:
-                col = row.findAll("td")
-                if len(col) != 5:
-                    continue
-                icon_url = col[0].find("img").get("src")
-                name = col[1].getText().strip()
-                stats = col[2].getText().strip()
-                arena_stats = col[3].getText().strip()
-                effect = col[4].getText().strip()
+            tables = soup.findAll("table", {"class": "pso2"})
+            for table in tables:
+                max_table_len = len(table.findAll("tr"))
+                for row in table.findAll("tr")[1:max_table_len]:
+                    col = row.findAll("td")
+                    icon_url = col[0].find("img").get("src")
+                    name = col[1].getText().strip()
+                    stats = col[2].getText().strip()
+                    arena_stats = col[3].getText().strip()
+                    effect = col[4].getText().strip()
 
-                en_name, _, jp_name = name.partition("\n")
-                cleaned_effect = re.sub(r"\n{2,}", r" ", effect)
-                cleaned_effect = cleaned_effect.replace("\n[\n", "[").replace(
-                    "\n]", "]"
-                )
-                ws = WeaponSymbol(
-                    en_name=en_name,
-                    jp_name=jp_name,
-                    base_stats=stats,
-                    arena_stats=arena_stats,
-                    effect=cleaned_effect,
-                    icon_url=icon_url,
-                )
-                self.weapon_symbols[en_name] = ws
-                self.weapon_symbols[jp_name] = ws
+                    en_name, _, jp_name = name.partition("\n")
+                    cleaned_effect = re.sub(r"\n{2,}", r" ", effect)
+                    cleaned_effect = cleaned_effect.replace("\n[\n", "[").replace(
+                        "\n]", "]"
+                    )
+                    ws = WeaponSymbol(
+                        en_name=en_name,
+                        jp_name=jp_name,
+                        base_stats=stats,
+                        arena_stats=arena_stats,
+                        effect=cleaned_effect,
+                        icon_url=icon_url,
+                    )
+                    self.weapon_symbols[en_name] = ws
+                    self.weapon_symbols[jp_name] = ws
+
+        finally:
+            await browser.close()
 
     async def import_soul_symbols(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(SOUL_DATABASE_URL) as r:
-                html = await r.text()
-        soup = BeautifulSoup(html, "html.parser")
+        browser = await launch(headless=True)
+        try:
+            page = await browser.newPage()
+            await page.goto(SOUL_DATABASE_URL)
+            html = await page.content()
+            soup = BeautifulSoup(html, "html.parser")
 
-        # Remove javascript and css from html
-        for script in soup(["script", "style"]):
-            script.extract()
+            # Remove javascript and css from html
+            for script in soup(["script", "style"]):
+                script.extract()
 
-        tables = soup.findAll("table", {"class": "pso2"})
-        for table in tables:
-            max_table_len = len(table.findAll("tr"))
-            req = False
-            row = table.findAll("tr")[0]
-            col = row.findAll("th")
-            if col[3].getText() == "Req.":
-                req = True
-            for row in table.findAll("tr")[1:max_table_len]:
-                col = row.findAll("td")
-                if len(col) != 5:
-                    continue
-                icon_url = col[0].find("img").get("src")
-                name = col[1].getText().strip()
-                stats = col[2].getText().strip()
+            tables = soup.findAll("table", {"class": "pso2"})
+            for table in tables:
+                max_table_len = len(table.findAll("tr"))
+                req = False
+                row = table.findAll("tr")[0]
+                col = row.findAll("th")
+                if col[3].getText() == "Req.":
+                    req = True
+                for row in table.findAll("tr")[1:max_table_len]:
+                    col = row.findAll("td")
+                    if len(col) != 5:
+                        continue
+                    icon_url = col[0].find("img").get("src")
+                    name = col[1].getText().strip()
+                    stats = col[2].getText().strip()
 
-                if req:
-                    requirements = col[3].getText().strip()
-                    arena_stats = None
-                else:
-                    requirements = None
-                    arena_stats = col[3].getText().strip()
+                    if req:
+                        requirements = col[3].getText().strip()
+                        arena_stats = None
+                    else:
+                        requirements = None
+                        arena_stats = col[3].getText().strip()
 
-                effect = col[4].getText().strip()
+                    effect = col[4].getText().strip()
 
-                en_name, _, jp_name = name.partition("\n")
-                en_name = en_name.strip()
-                jp_name = jp_name.strip()
-                cleaned_effect = re.sub(r"\n{2,}", r" ", effect)
-                cleaned_effect = cleaned_effect.replace("\n[\n", "[").replace(
-                    "\n]", "]"
-                )
-                ss = SoulSymbol(
-                    en_name=en_name,
-                    jp_name=jp_name,
-                    requirements=requirements,
-                    base_stats=stats,
-                    arena_stats=arena_stats,
-                    effect=cleaned_effect,
-                    icon_url=icon_url,
-                )
-                self.soul_symbols[en_name] = ss
-                self.soul_symbols[jp_name] = ss
+                    en_name, _, jp_name = name.partition("\n")
+                    en_name = en_name.strip()
+                    jp_name = jp_name.strip()
+                    cleaned_effect = re.sub(r"\n{2,}", r" ", effect)
+                    cleaned_effect = cleaned_effect.replace("\n[\n", "[").replace(
+                        "\n]", "]"
+                    )
+                    ss = SoulSymbol(
+                        en_name=en_name,
+                        jp_name=jp_name,
+                        requirements=requirements,
+                        base_stats=stats,
+                        arena_stats=arena_stats,
+                        effect=cleaned_effect,
+                        icon_url=icon_url,
+                    )
+                    self.soul_symbols[en_name] = ss
+                    self.soul_symbols[jp_name] = ss
+        finally:
+            await browser.close()
 
     def get_unfuzzed_weapon_name(self, weapon_name):
         unfuzz_weapon_name, score = process.extractOne(
@@ -188,7 +195,7 @@ class BumpedParser(object):
 
     def get_weapon_database(self):
         return set(
-            weapon_symbol.en_name for weapon_symbol in self.weapon_symbol.values()
+            weapon_symbol.en_name for weapon_symbol in self.weapon_symbols.values()
         )
 
     def get_soul_database(self):
