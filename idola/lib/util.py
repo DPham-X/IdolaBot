d59efc48674d85f4d541ae44fdbb95eb5e44d4ba
Dzuy Pham <pham.dzuy@gmail.com>
2021-10-18 21:01:56 +1100

    style: Add types on util functions

--- idola/lib/util.py
+++ idola/lib/util.py
@@ -3,11 +3,11 @@ from urllib.parse import urlencode
 from urllib.request import urlopen
 
 
-def shorten_url(url):
+def shorten_url(url: str) -> str:
     request_url = "http://tinyurl.com/api-create.php?" + urlencode({"url": url})
     with contextlib.closing(urlopen(request_url)) as response:
-        return response.read().decode("utf-8 ")
+        return response.read().decode("utf-8")
 
 
-def base_round(x: int, base=1):
+def base_round(x: int, base: int = 1) -> int:
     return base * round(int(x) / base)
